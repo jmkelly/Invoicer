ace122e25237067ce32a3a325a4b136ef3afe219
James Kelly <james.kelly.99@gmail.com>
2023-09-02 11:59:37 +1000

    basic db context

---
+++ Invoicer.Web/DbContext.cs
@@ -0,0 +1,22 @@
+namespace WebApi.Helpers;
+
+using Invoicer.Web.Pages.Creditors.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to postgres with connection string from app settings
+        options.UseNpgsql(Configuration.GetConnectionString("Default"));
+    }
+
+    public DbSet<Creditor> Creditors { get; set; }
+}
