c5ef90a52ccf5f1a09863956e525e22972759d79
James Kelly <james.kelly.99@gmail.com>
2023-09-02 21:15:02 +1000

    not quite yet

--- Invoicer.Web/Pages/Work/Create.cs
+++ Invoicer.Web/Pages/Work/Create.cs
@@ -1,10 +1,68 @@
+using Humanizer;
+using Invoicer.Web.Pages.Clients;
+using Invoicer.Web.Pages.Clients.Models;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Invoicer.Web.Pages.Work;
 public class Create : PageModel
 {
-    public void OnGet()
+    private readonly DataContext context;
+
+    public Create(DataContext context)
     {
+        this.context = context;
     }
+
+    [BindProperty]
+    public Guid ClientId {get;set;}
+
+    public CreateWorkModel Model {get;set;}
+
+    public  List<SelectListItem> Options {get;set;}
+    public async Task OnGet()
+    {
+        var clients =  await context.Clients.ToListAsync();
+        Options = clients.Select(c => new SelectListItem {
+            Value = c.Id.ToString(),
+            Text = c.Name
+        }).ToList();
+        
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        //convert the work
+
+
+        return RedirectToPage("./Index");
+    }
+}
+
+public class CreateWorkModel : Work,  IModel
+{
+}
+
+public class Work : Entity 
+{
+    public DateTime Date {get;set;}
+    public decimal Hours {get;set;}
+    public required string Description {get;set;}
+    public decimal Rate {get;set;}
+    public RateUnits RateUnits {get;set;}
+    public Guid ClientId {get;set;}
+}
+
+public enum RateUnits
+{
+    PerHour,
+    PerDay
 }
