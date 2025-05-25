1f3cb3c19fdd087ba929971644381453cab29b54
James Kelly <james.kelly.99@gmail.com>
2025-05-25 20:59:41 +1000

    add app db context

--- Invoicer.Web/Program.cs
+++ Invoicer.Web/Program.cs
@@ -7,10 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<DataContext>(options =>
-        {
-            options.EnableSensitiveDataLogging();
-            options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
-        });
+		{
+			options.EnableSensitiveDataLogging();
+			options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+		});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+		{
+
+			options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+		});
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+	{
+		options.SignIn.RequireConfirmedAccount = false;
+	})
+	.AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 
@@ -20,9 +34,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
