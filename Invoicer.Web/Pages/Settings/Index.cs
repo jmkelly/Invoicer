2a5178de04dbea91cf3b97d23ce6c34f93783eae
James Kelly <james.kelly.99@gmail.com>
2025-05-25 17:03:52 +1000

    format and add required

--- Invoicer.Web/Pages/Settings/Index.cs
+++ Invoicer.Web/Pages/Settings/Index.cs
@@ -6,57 +6,57 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Invoicer.Web.Pages.Settings
 {
-    public class Index : PageModel 
-    {
-        [BindProperty]
-        public SettingModel Model {get;set;}
+	public class Index : PageModel
+	{
+		[BindProperty]
+		public required SettingModel Model { get; set; }
 
-        public Index(DataContext context, ILogger<Index> logger)
-        {
-            Context = context;
-            Logger = logger;
-        }
+		public Index(DataContext context, ILogger<Index> logger)
+		{
+			Context = context;
+			Logger = logger;
+		}
 
-        public DataContext Context { get; }
-        public ILogger<Index> Logger { get; }
+		public DataContext Context { get; }
+		public ILogger<Index> Logger { get; }
 
-        public async Task<IActionResult> OnGet()
-        {
-            var entity = await Context.Settings.FirstOrDefaultAsync();
+		public async Task<IActionResult> OnGet()
+		{
+			var entity = await Context.Settings.FirstOrDefaultAsync();
 
-            if (entity != null)
-            {
-                Model = entity.Adapt<SettingModel>();
-            }
-            Logger.LogInformation("{entity}", entity);
+			if (entity != null)
+			{
+				Model = entity.Adapt<SettingModel>();
+			}
+			Logger.LogInformation("{entity}", entity);
 
-            return Page();
-        }
+			return Page();
+		}
 
-        public async Task<IActionResult> OnPost()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+		public async Task<IActionResult> OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
-            var entity = Model.Adapt<Setting>();
+			var entity = Model.Adapt<Setting>();
 
-            if (Model.Id == default)
-            {
-                Logger.LogInformation("no Id for {@entity}, new entity ",entity);
-                entity.Id = NewId.NextSequentialGuid();
-                Context.Add(entity);
-            }
-            else
-            {
-                Logger.LogInformation("{@entity} exists updating",entity);
-                Context.Update(entity);
-            }
+			if (Model.Id == default)
+			{
+				Logger.LogInformation("no Id for {@entity}, new entity ", entity);
+				entity.Id = NewId.NextSequentialGuid();
+				Context.Add(entity);
+			}
+			else
+			{
+				Logger.LogInformation("{@entity} exists updating", entity);
+				Context.Update(entity);
+			}
 
-            await Context.SaveChangesAsync();
-            return RedirectToPage("./Index");
-        }
+			await Context.SaveChangesAsync();
+			return RedirectToPage("./Index");
+		}
 
-    }
+	}
 }
