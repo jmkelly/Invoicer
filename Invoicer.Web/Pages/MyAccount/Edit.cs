c8ac6c7717486d2837024c2589c93772fa4f28c8
James Kelly <james.kelly.99@gmail.com>
2025-05-25 17:02:18 +1000

    add edit page for accounts

---
+++ Invoicer.Web/Pages/MyAccount/Edit.cs
@@ -0,0 +1,10 @@
+namespace Invoicer.Web.Pages.MyAccount
+{
+	public class Edit : EditPageModel<MyAccountEditModel, MyAccount>
+	{
+		public Edit(DataContext context, ILogger<EditPageModel<MyAccountEditModel, MyAccount>> logger) : base(context, logger)
+		{
+			logger.LogInformation("Edit Account");
+		}
+	}
+}
