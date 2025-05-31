e825f7dd303017ba306fe1c930d449f50bb77337
James Kelly <james.kelly.99@gmail.com>
2025-05-31 17:01:13 +1000

    format

--- Invoicer.Web/Pages/WorkItems/CreateWorkItemModel.cs
+++ Invoicer.Web/Pages/WorkItems/CreateWorkItemModel.cs
@@ -1,11 +1,14 @@
+using Invoicer.Web.Pages.Clients.Models;
+
 namespace Invoicer.Web.Pages.WorkItems;
 
 public class CreateWorkItemModel
 {
-    public DateOnly Date {get;set;}
-    public decimal Hours {get;set;}
-    public required string Description {get;set;}
-    public decimal Rate {get;set;}
-    public RateUnits RateUnits {get;set;}
-    public Guid ClientId {get;set;}
+	public DateOnly Date { get; set; }
+	public decimal Hours { get; set; }
+	public required string Description { get; set; }
+	public decimal Rate { get; set; }
+	public RateUnits RateUnits { get; set; }
+	public Guid ClientId { get; set; }
+	public required List<Client> Clients { get; set; }
 }
