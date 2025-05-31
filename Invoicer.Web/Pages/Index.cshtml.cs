c8497bbfb3203ed716e621383d7304dac88d6e0a
James Kelly <james.kelly.99@gmail.com>
2025-05-31 17:01:50 +1000

    add form for adding hours

--- Invoicer.Web/Pages/Index.cshtml.cs
+++ Invoicer.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Invoicer.Web.Pages.Invoices;
+using Invoicer.Web.Pages.WorkItems;
 
 
 namespace Invoicer.Web.Pages;
@@ -9,7 +9,7 @@ public class IndexModel : PageModel
 {
 	private readonly ILogger<IndexModel> _logger;
 	private readonly DataContext context;
-	public required InvoicesIndexModel InvoicesIndexModel { get; set; }
+	public IndexViewModel IndexViewModel { get; set; }
 
 	public IndexModel(ILogger<IndexModel> logger, DataContext context)
 	{
@@ -33,17 +33,27 @@ public class IndexModel : PageModel
 			.Where(w => !w.HasInvoice()).ToList();
 
 
-		var model = new InvoicesIndexModel();
-		foreach (var invoice in invoices)
-		{
-			model.Invoices.Add(invoice);
-		}
+		var clients = await context.Clients.ToListAsync();
 
-		foreach (var wi in workItems)
+		var wi = new CreateWorkItemModel
 		{
-			model.OutstandingWorkItems.Add(wi);
-		}
+			Date = DateOnly.FromDateTime(DateTime.Now),
+			Hours = 0,
+			Description = string.Empty,
+			Rate = 0,
+			ClientId = default,
+			Clients = clients,
+			RateUnits = RateUnits.PerHour,
+		};
 
-		InvoicesIndexModel = model;
+		var model = new IndexViewModel
+		{
+			Invoices = invoices,
+			OutstandingWorkItems = workItems,
+			NewWorkItem = wi
+		};
+
+
+		IndexViewModel = model;
 	}
 }
