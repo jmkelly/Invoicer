08ae3f68e392c1bad39c1cdf31a344652a21fe0b
James Kelly <james.kelly.99@gmail.com>
2025-05-31 17:01:43 +1000

    add model for adding hours

--- Invoicer.Web/Pages/InvoicesIndexModel.cs
+++ Invoicer.Web/Pages/InvoicesIndexModel.cs
@@ -1,15 +1,18 @@
-namespace Invoicer.Web.Pages.Invoices;
+namespace Invoicer.Web.Pages;
+
+using Invoicer.Web.Pages.Invoices;
 using Invoicer.Web.Pages.WorkItems;
 
-public class InvoicesIndexModel 
+public class IndexViewModel
 {
-    public InvoicesIndexModel()
-    {
+	public IndexViewModel()
+	{
 		Invoices = new List<Invoice>();
 		OutstandingWorkItems = new List<WorkItem>();
-    }
+	}
 
-    public List<Invoice> Invoices {get;set;}	
-	public List<WorkItem> OutstandingWorkItems {get;set;}
+	public List<Invoice> Invoices { get; set; }
+	public List<WorkItem> OutstandingWorkItems { get; set; }
+	public required CreateWorkItemModel NewWorkItem { get; set; }
 }
 
