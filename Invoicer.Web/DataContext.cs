86ba2a72cf0cb7e73e401c2d39d97d46f14746e7
James Kelly <james.kelly.99@gmail.com>
2025-05-31 17:02:21 +1000

    format

--- Invoicer.Web/DataContext.cs
+++ Invoicer.Web/DataContext.cs
@@ -1,4 +1,5 @@
 namespace Invoicer.Web;
+
 using Invoicer.Web.Pages.Clients.Models;
 using Invoicer.Web.Pages.Invoices;
 using Invoicer.Web.Pages.MyAccount;
@@ -8,22 +9,22 @@ using Microsoft.EntityFrameworkCore;
 
 public class DataContext : DbContext
 {
-		protected readonly IConfiguration Configuration;
+	protected readonly IConfiguration Configuration;
 
-    public DataContext(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+	public DataContext(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // connect to postgres with connection string from app settings
-        options.UseNpgsql(Configuration.GetConnectionString("Default"));
-    }
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+	{
+		// connect to postgres with connection string from app settings
+		options.UseNpgsql(Configuration.GetConnectionString("Default"));
+	}
 
-    public DbSet<Client> Clients { get; set; }
-    public DbSet<MyAccount> MyAccounts { get; set; }
+	public DbSet<Client> Clients { get; set; }
+	public DbSet<MyAccount> MyAccounts { get; set; }
 	public DbSet<Setting> Settings { get; set; }
-    public DbSet<WorkItem> WorkItems { get; set; }
-    public DbSet<Invoice> Invoices { get; set; }
+	public DbSet<WorkItem> WorkItems { get; set; }
+	public DbSet<Invoice> Invoices { get; set; }
 }
