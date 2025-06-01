8008f8630a81458f950ffd1af5a9d6abb6c5935e
James Kelly <james.kelly.99@gmail.com>
2025-06-01 20:16:51 +1000

    implement create invoice route

--- Invoicer.Web/Pages/Index.cshtml.cs
+++ Invoicer.Web/Pages/Index.cshtml.cs
@@ -98,4 +98,60 @@ public class IndexModel : PageModel
 		await context.SaveChangesAsync();
 		return RedirectToPage("Index");
 	}
+
+	public async Task<ActionResult> OnPostCreateInvoice()
+	{
+		logger.LogInformation(IndexViewModel.ToJson());
+		InvoiceAddModel? vm = IndexViewModel.NewInvoice;
+		if (vm is null)
+		{
+			logger.LogWarning("vm is null");
+			return RedirectToPage("Index");
+		}
+
+		List<Guid> ids = vm.OutStandingWorkItems.Where(c => c.IsSelected).Select(c => c.Id).ToList();
+		List<WorkItem> wi = await context.WorkItems
+			.Include(c => c.Client)
+			.Where(c => ids.Any(h => h == c.Id))
+			.ToListAsync();
+		MyAccount.MyAccount? account = await context.MyAccounts.FirstOrDefaultAsync(c => c.Id == vm.SelectedAccountId);
+
+		if (account is null)
+		{
+			logger.LogWarning("account {id} not found", vm.SelectedAccountId);
+			return RedirectToPage("Index");
+		}
+
+		//select the distinct clients
+		List<Clients.Models.Client?>? clients = wi.Select(c => c.Client).Distinct().ToList();
+		if (clients is null)
+		{
+			logger.LogWarning("clients is null");
+			return RedirectToPage("Index");
+		}
+
+		foreach (Clients.Models.Client? client in clients)
+		{
+			if (client is not null)
+			{
+				Invoice invoice = new Invoice
+				{
+					Client = client,
+					CreatedAt = DateTime.UtcNow,
+					InvoiceCode = Invoice.CreateInvoiceCode(client.Name, DateTime.UtcNow),
+					InvoiceStatus = InvoiceStatus.Created,
+					Account = account,
+				};
+				foreach (WorkItem? item in wi.Where(c => c.Client == client))
+				{
+					invoice.AddWorkItem(item);
+					context.Invoices.Add(invoice);
+				}
+			}
+		}
+
+		await context.SaveChangesAsync();
+		return RedirectToPage("Index");
+
+	}
 }
