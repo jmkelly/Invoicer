d450391f892ed80e9f97ea7c8ccb3cf94209af29
James Kelly <james.kelly.99@gmail.com>
2025-06-01 14:05:40 +1000

    add more items from page to the model

--- Invoicer.Web/Pages/Invoices/InvoiceAddModel.cs
+++ Invoicer.Web/Pages/Invoices/InvoiceAddModel.cs
@@ -1,11 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace Invoicer.Web.Pages.Invoices
 {
-    public class InvoiceAddModel
-    {
-        public Guid ClientId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpddatedAt { get; set; }
-        public required List<WorkItemInvoiceModel> SelectedWorkItems { get; set; }
-    }
+	public class InvoiceAddModel
+	{
+		public Guid ClientId { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpddatedAt { get; set; }
+		public required List<WorkItemInvoiceModel> SelectedWorkItems { get; set; }
+		public required List<WorkItemInvoiceModel> OutStandingWorkItems { get; set; }
+		public required List<MyAccount.MyAccount> Accounts { get; set; }
+		public Guid SelectedAccountId { get; set; }
+	}
+
+	public static class InvoiceAddModelExtensions
+	{
+		public static List<SelectListItem> ToSelectItemList(this List<MyAccount.MyAccount> accounts)
+		{
+			return accounts.Select(n => new SelectListItem { Text = n.Label(), Value = n.Id.ToString() }).ToList();
+		}
+	}
 }
 
