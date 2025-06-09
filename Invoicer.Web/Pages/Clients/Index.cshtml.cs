d218ee6a3d997146326dd61820522b867e668539
James Kelly <james@locationsolve.com>
2025-06-09 16:53:37 +1000

    implemnt infinite load

--- Invoicer.Web/Pages/Clients/Index.cshtml.cs
+++ Invoicer.Web/Pages/Clients/Index.cshtml.cs
@@ -1,4 +1,6 @@
+using Htmx;
 using Mapster;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,10 +16,31 @@ namespace Invoicer.Web.Pages.Clients
 		}
 
 		public required List<ClientIndexModel> Clients { get; set; }
-		public async Task OnGet()
+		public int PageSize { get; set; } = 20;
+		public int PageNum { get; set; } = 1;
+		public string Search { get; set; } = string.Empty;
+
+		public async Task<IActionResult> OnGetAsync(string search = "", int pageNum = 1, int pageSize = 20)
 		{
-			var clients = await context.Clients.ToListAsync();
-			Clients = clients.Select(c => ClientIndexModel.Create(c)).ToList();
+			var skip = (pageNum - 1) * pageSize;
+			PageNum = pageNum;
+			PageSize = pageSize;
+			Search = search ?? string.Empty;
+
+			var clients = await context.Clients.OrderBy(c => c.Name).Skip(skip).Take(pageSize).ToListAsync();
+			if (string.IsNullOrWhiteSpace(Search))
+			{
+				clients = [.. clients.Where(c => c.SearchString.Contains(Search, StringComparison.InvariantCultureIgnoreCase))];
+			}
+
+			var isHtmx = HttpContext.Request.IsHtmx();
+
+			Clients = clients.Select(ClientIndexModel.Create).ToList();
+
+			return Request.IsHtmx()
+				? Partial("_ClientRowsPartial", this)
+				: Page();
+
 		}
 	}
 }
