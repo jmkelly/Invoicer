2f6fa844ef4ef63d74fe87c05f7f80328e490068
James Kelly <james@locationsolve.com>
2025-06-09 16:52:52 +1000

    add property for implementing search

--- Invoicer.Web/Pages/Clients/Models/Client.cs
+++ Invoicer.Web/Pages/Clients/Models/Client.cs
@@ -1,15 +1,17 @@
 namespace Invoicer.Web.Pages.Clients.Models
 {
-    public class Client : Entity
-    {
-        public string? CompanyName { get; set; }
-        public required string Name { get; set; }
-        public string? BSB { get; set; }
-        public string? AccountNo { get; set; }
-        public string? StreetNumber { get; set; }
-        public string? Street { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? Postcode { get; set; }
-    }
+	public class Client : Entity
+	{
+		public string? CompanyName { get; set; }
+		public required string Name { get; set; }
+		public string? BSB { get; set; }
+		public string? AccountNo { get; set; }
+		public string? StreetNumber { get; set; }
+		public string? Street { get; set; }
+		public string? City { get; set; }
+		public string? State { get; set; }
+		public string? Postcode { get; set; }
+
+		public string SearchString => $"{CompanyName} {Name} {BSB} {AccountNo} {StreetNumber} {Street} {City} {State} {Postcode}".ToLower();
+	}
 }
