8efea806fe9f091a7844d54691675d4092ac040b
James Kelly <james@locationsolve.com>
2025-06-15 12:45:04 +1000

    implement auto search

--- Invoicer.Web/Pages/Clients/Index.cshtml.cs
+++ Invoicer.Web/Pages/Clients/Index.cshtml.cs
@@ -1,39 +1,47 @@
 using Htmx;
-using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
 namespace Invoicer.Web.Pages.Clients
 {
-	public class IndexModel : PageModel
+	public class IndexModel(DataContext context, ILogger<IndexModel> logger) : PageModel
 	{
-		private readonly DataContext context;
-
-		public IndexModel(DataContext context)
-		{
-			this.context = context;
-		}
+		private readonly DataContext context = context;
+		private readonly ILogger<IndexModel> logger = logger;
 
 		public required List<ClientIndexModel> Clients { get; set; }
 		public int PageSize { get; set; } = 20;
 		public int PageNum { get; set; } = 1;
+		[BindProperty(SupportsGet = true)]
 		public string Search { get; set; } = string.Empty;
 
-		public async Task<IActionResult> OnGetAsync(string search = "", int pageNum = 1, int pageSize = 20)
+		public async Task<IActionResult> OnGetAsync(string? search, int? pageNum, int? pageSize)
 		{
-			var skip = (pageNum - 1) * pageSize;
-			PageNum = pageNum;
-			PageSize = pageSize;
+			PageSize = pageSize ?? 20;
+			PageNum = pageNum ?? 1;
+			var skip = (PageNum - 1) * PageSize;
 			Search = search ?? string.Empty;
 
-			var clients = await context.Clients.OrderBy(c => c.Name).Skip(skip).Take(pageSize).ToListAsync();
-			if (string.IsNullOrWhiteSpace(Search))
+			var clients = context.Clients.AsQueryable();
+
+			if (!string.IsNullOrWhiteSpace(Search))
 			{
-				clients = [.. clients.Where(c => c.SearchString.Contains(Search, StringComparison.InvariantCultureIgnoreCase))];
+				logger.LogInformation("searching for {Search}", Search);
+				//this isn't ideal.  If we keep to pg, then usen EF.Function.Ilike but i currently
+				//plan to switch to sqlite so this may still be need to be kept
+				clients = clients.Where(c =>
+						c.Name.ToLower().Contains(Search.ToLower()) ||
+						c.CompanyName!.ToLower().Contains(Search.ToLower()) ||
+						c.Street!.ToLower().Contains(Search.ToLower()) ||
+						c.City!.ToLower().Contains(Search.ToLower())
+						);
 			}
 
-			Clients = clients.Select(ClientIndexModel.Create).ToList();
+			var clientsList = await clients.OrderBy(c => c.Name).Skip(skip).Take(PageSize).ToListAsync();
+
+			Clients = [.. clientsList.Select(ClientIndexModel.Create)];
+
 
 			return Request.IsHtmx()
 				? Partial("_ClientRowsPartial", this)
